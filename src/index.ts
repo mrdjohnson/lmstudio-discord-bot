e3eeacdb741a0414a54db6a3911e98bf379aaa2e
djohnson <mr.demarcus.johnson@gmail.com>
2024-06-20 17:33:26 +0700

    Adding a 'Ping' command to Discord

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,54 @@
 import 'dotenv/config';
 import { LMStudioClient } from '@lmstudio/sdk';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder } from 'discord.js';
 
 // ! in typescript just says these are not null
 const CLIENT_TOKEN = process.env.CLIENT_TOKEN!;
 const CLIENT_ID = process.env.CLIENT_ID!;
 const GUILD_ID = process.env.GUILD_ID!;
+
+function createDiscordSlashCommands() {
+  const pingCommand = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('A simple check to see if I am available')
+    .toJSON();
+
+  const allCommands = [
+    pingCommand
+  ];
+
+  // Gives a pretty-print view of the commands
+  console.log();
+  console.log(JSON.stringify(allCommands, null, 2));
+  console.log();
+
+  return allCommands;
+}
+
+// We send our commands to discord so it knows what to look for
+async function activateDiscordSlashCommands() {
+  const rest = new REST({ version: '10' }).setToken(CLIENT_TOKEN);
+
+  try {
+    console.log('Started refreshing bot (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: createDiscordSlashCommands()
+    });
+
+    console.log('Successfully reloaded bot (/) commands.');
+  } catch (error) {
+    console.error(error);
+
+    return false;
+  }
+
+  console.log();
+
+  return true;
+}
+
 
 async function main() {
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -16,5 +59,10 @@ async function main() {
 
   client.login(CLIENT_TOKEN);
 }
+
+// // uncomment this if you want to test the slash command activation
+// activateDiscordSlashCommands().then(() => {
+//   console.log('Finished activating Discord / Commands');
+// });
 
 main();
