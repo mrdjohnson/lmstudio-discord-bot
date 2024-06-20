012548a62d6ab49024c6aea6334c55cb3b8677a7
djohnson <mr.demarcus.johnson@gmail.com>
2024-06-20 17:32:16 +0700

    Logging our bot into Discord

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { LMStudioClient } from '@lmstudio/sdk';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 // ! in typescript just says these are not null
 const CLIENT_TOKEN = process.env.CLIENT_TOKEN!;
@@ -7,6 +8,13 @@ const CLIENT_ID = process.env.CLIENT_ID!;
 const GUILD_ID = process.env.GUILD_ID!;
 
 async function main() {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+  });
+
+  client.login(CLIENT_TOKEN);
 }
 
 main();
